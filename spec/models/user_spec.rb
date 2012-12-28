44756fc0ec7e72d6bf34bac84d0ddf2588e3e5cf
Alexander Weber <AlexWe@Alexander-Webers-MacBook-Pro.local>
2012-12-28 10:45:43 +0200

    Finished updating users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,9 +15,20 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }  
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -114,5 +125,14 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar", admin: true)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
   end
 end
